207286986d4577e6d2928c399090ee6e6310f168
ali <aallii.r98@gmail.com>
2021-10-24 23:46:34 +0330

    read files and save matrix

--- code/src/main/Main.java
+++ code/src/main/Main.java
@@ -6,9 +6,45 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
+        matrix matrix = read_matrix();
     }
 
+    static matrix read_matrix() throws IOException {
+        FileReader fin = new FileReader("./M(300,200).csv");
+        Scanner sc = new Scanner(fin);
+        ArrayList<node> nodes = new ArrayList<node>();
+        node node2 = null;
+        int j = -1;
+        while (sc.hasNext()) {
+            String[] arr = sc.nextLine().split(",");
+
+            if (j == nodes.size()) {
+                node2 = new node(-2, -2, null);
+                nodes.add(node2);
+            }
+            if (nodes.size() > 0)
+                node2.pointer = null;
+            j = nodes.size();
+            for (int i = 0; i < arr.length; i++)
+                if (!arr[i].equals("0")) {
+                    node node1 = new node(-1, -1, null);
+                    node node = new node(i, Integer.parseInt(arr[i]), node1);
+
+                    if (nodes.size() > 0 && node2.pointer != null)
+                        node2.pointer = node;
+
+                    if (nodes.size() > 0 && node2.pointer == null)
+                        nodes.add(node);
+
+                    if (nodes.size() == 0)
+                        nodes.add(node);
+
+                    node2 = node;
+                }
+        }
+        nodes.get(nodes.size() - 1).pointer = null;
+        return new matrix(nodes);
+    }
 
 
 }
